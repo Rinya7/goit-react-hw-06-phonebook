e9126d9f3013b388b8a307d3ae19602c5ce67bd5
Rinya7 <v.dmitriev0712@gmail.com>
2023-08-05 20:26:14 +0200

    Final

--- src/components/Redux/selectors.js
+++ src/components/Redux/selectors.js
@@ -3,10 +3,9 @@ export const selectFilter = state => state.filters;
 
 export const selectVisibleContacts = ({ contacts: { contacts }, filters }) => {
   if (!filters) {
-    console.log(null);
     return contacts;
   }
-  console.log(true);
+
   return contacts.filter(contact =>
     contact.name.toLowerCase().includes(filters.toLowerCase())
   );
